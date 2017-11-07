2e86035334b46dfa38240ce5c1df5bb56c96b8f9
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2017-11-07 11:00:21 +0100

    fini

--- function.php
+++ function.php
@@ -39,5 +39,87 @@ function initiales_de($mots)
   return $initiales;
 }
 echo "<br>";
-echo initiales_de("In code we trust!");
- ?>
+echo initiales_de("Ceci est un test");
+echo "<br>";
+
+function remplace_ae($mot)
+{
+  $mot=str_split($mot);
+  //$last_was_a=false;
+  for ($i=0; $i <count($mot)-1 ; $i++) {
+    if ($mot[$i] == "a") {
+      if ($i <count($mot)) {//s'il reste une lettre après
+        if ($mot[$i+1] == "e") {
+          $mot[$i]="æ";
+          //unset($mot[$i+1]);
+          $mot[$i+1]="";
+          $i++;
+        }
+      }
+    }
+  }
+
+  return implode($mot);
+}
+echo remplace_ae("aeaeaeae");
+
+function remplace_æ($mot)
+{
+  return str_replace("æ","ae",$mot);
+}
+echo "<br>";
+
+echo str_replace("æ","ae","chænichthys");
+
+function feedback($message,$classe="info")
+{
+  echo "<div class=\"{$classe}\"> {$message} </div>";
+}
+feedback("Ceci est une erreur","Warning");
+feedback("Ceci est une erreur sans classe");
+
+function word_generator($min,$max)
+{
+  $nbr_letter=rand($min,$max);
+  $word_array=[];
+  while ($nbr_letter >0) {
+    $nbr_letter--;
+    array_push($word_array,chr(rand(97,122)));
+  }
+  return implode($word_array);
+}
+echo "<br>  <br>";
+echo "<a href=\"function.php\">recommencer</a>";
+echo "<br>";
+echo word_generator(1,5);
+echo "<br>";
+echo word_generator(7,15);
+$en_minuscule=strtolower("ARRÊTE DE CRIER JE N'ENTENDS PLUS RIEN!!!");
+
+function reverse_string($stringToReverse){
+    $stringToReverse=str_split($stringToReverse);
+    $reversed=[];
+    for ($i=count($stringToReverse)-1; $i >=0 ; $i--) {
+      array_push($reversed,$stringToReverse[$i]);
+    }
+    return implode($reversed);
+}
+echo reverse_string("truc");
+echo "<br>  <br>";
+
+$chanson="A
+Bavas a ca ma sarpata a parda,
+Ma la macha ma la macha
+Bavas a ca ma sarpata a parda
+Ma la macha a ravana.
+";
+$substitutions = array( 'E', 'I', 'O', 'U', 'OU', 'É', 'È', 'OI', 'UI', 'OUI', 'AN', 'IN', 'ON', 'UN', 'OIN');
+echo "<br>  <br>";
+echo "<br>  <br>";
+
+for ($i=0; $i <count($substitutions)-1 ; $i++) {
+  echo str_replace("a",$substitutions[$i],$chanson);
+  echo "<br>  <br>";
+
+}
+?>
